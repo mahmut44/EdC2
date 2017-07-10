839d2f806c2cf70058bfbd3620b3a50de807239e
GULCU Mahmut <Mahmut.GULCU@cegedim.com>
2017-07-10 16:29:32 +0200

    4eme commit

--- EDC2017 script NEO4J.sql
+++ EDC2017 script NEO4J.sql
@@ -59,14 +59,101 @@ RETURN r,z,y;
 
 
 
+API REST:
+
+
+POST http://localhost:7474/db/data/cypher
+Accept: application/json; charset=UTF-8
+Content-Type: application/json
+
+
+{
+  "query" : "START n=node(*) MATCH (u:User)-[r:SENT]->(m:Message)
+RETURN u, r, count(r)
+ORDER BY count(r)
+LIMIT 10",
+  "params" : {
+    "startName" : "I",
+    "name" : "you"
+  }
+}
 
 
 
+a) Nombre de relation follows
+
+MATCH (u:User)-[r:FOLLOWS]->(z:User)
+WITH u, count(z) as nbrfol, collect(z) as users
+RETURN u,nbrfol,users
+ORDER by nbrfol DESC limit 1; -- si on souhaite le MAX de message envoyé par une personne
+
+--> Le nombre de FOLLOWS MAX = 4 par Charle
+
+b) Nombre de message créé par un utilsateur
+
+MATCH (u:User)-[r:SENT]->(z:Message)
+WITH u, count(z) as nbrmes, collect(z) as messages
+RETURN u,nbrmes,messages;
+ORDER BY nbrmes DESC LIMIT 1; --Si on veut la personne qui a le plus de  messages envoyés
+
+
+--> RESULTAT MAX ELINE = 22 mesages envoyés
+
+
+c) nbr de forward max
+
+MATCH (u:Message)<-[r:FORWARD]-(z:Message)
+WITH u, count(z) as nbrfor, collect(z) as messages
+RETURN u,nbrfor,messages
+
+--> RESULTAT MAX forword message 10 forwardé 3 fois et envoyé par ELISE
+
+
+a) API REST 
+
+POST http://localhost:7474/db/data/cypher
+Accept: application/json; charset=UTF-8
+Content-Type: application/json
+{
+  "query" : "MATCH (u:User)-[r:FOLLOWS]->(z:User)
+WITH u, count(z) as nbrfol, collect(z) as users
+RETURN u,nbrfol,users",
+}
+
+b) API REST
+
+
+POST http://localhost:7474/db/data/cypher
+Accept: application/json; charset=UTF-8
+Content-Type: application/json
+{
+  "query" : "MATCH (u:User)-[r:SENT]->(z:Message)
+WITH u, count(z) as nbrmes, collect(z) as messages
+RETURN u,nbrmes,messages;
+ORDER BY nbrmes DESC LIMIT 1",
+}
+
+c) API REST 
+
+POST http://localhost:7474/db/data/cypher
+Accept: application/json; charset=UTF-8
+Content-Type: application/json
+{
+  "query" : "MATCH (u:Message)<-[r:FORWARD]-(z:Message)
+WITH u, count(z) as nbrfor, collect(z) as messages
+RETURN u,nbrfor,messages",
+}
+
+-- Elise est le leader de l opinion principale. Son message a été forwardé 3 fois.
+-- Marc est lui aussi un leader du fait qu'il a le plus  d'abonnés.
 
 
 
+5) Identifier 2 utilisaeur Points communs, creer relation know
 
-
-
-
-
+	MATCH (p:User)-[l:LIKES]->(z)
+	WITH u, count(z) as nbrlikes, collect(z) as item
+	RETURN u,nbrlikes,item
+	
+	
+	-- Olivier et Laura
